0554a55937757a91721d71c3b59323c6c76029a3
Jake Harris <milieu@javakat.io>
2015-02-11 22:47:52 -0600

    Sometimes my commits have poetry in them

--- source/Assets/Scripts/Player/Flashlight.cs
+++ source/Assets/Scripts/Player/Flashlight.cs
@@ -7,7 +7,10 @@ public class Flashlight : MonoBehaviour {
 
 	public float intensity = 3.87f;
 
-	// Update is called once per frame
+	void Awake () {
+		this.transform.light.intensity = intensity;
+	}
+
 	void Update () {
 		if (Input.GetButtonUp ("Flashlight")) {
 			this.transform.light.intensity = (IsOn()) ? 0 : intensity;
@@ -22,4 +25,41 @@ public class Flashlight : MonoBehaviour {
 	private bool IsOn() {
 		return this.transform.light.intensity != 0;
 	}
+
+	// NOTES:
+	// In the future I'll want to make this flicker.
+	// To do so, I think I want the following:
+	// - float timer -> Every <timer> seconds, generate a random number.
+	// - float repeatTimer -> After <repeatTimer> seconds, stop 
+	//                        increasing the chances of a repeat flicker.
+
+	// - if we <are flickering>, 
+	// --- set <light.intensity> to be <intensity>
+	// --- and continue;
+
+	// - if random number plus a repeat modifier is above threshold
+	// --- then we are flickering,
+	// --- an infinite cast of torches, perfectly arranged,
+	// --- sordid rows of sorted rose.
+	// --- and it's scary, that's the truth of it.
+	// --- so turn the lights out,
+	// --- dim us down, 
+	// --- hear us quiet and lap at the dark,
+	// --- offcolor flames each armed to the teeth
+	// --- with mountainous flowers and a flourish of heat.
+	// --- suck it dry, rubbing the whole damn thing
+	// --- we flicker, not ours, losing self,
+	// --- together.
+	// --- here, in order, lives the greatest chaos,
+	// --- and we are not dead yet.
+
+	// --- ...
+	// --- Anyways.
+	// --- then we are flickering (<isFlickering>),
+	// --- so turn <light.intensity> to 0,
+	// --- and,
+	// --- if <repeat modifier> is greater than 0,
+	// ----- subtract <usage amount> from <repeat modifier>.
+	// --- otherwise,
+	// ----- set <repeat modifier> to <repeat modifier base amount>.
 }
